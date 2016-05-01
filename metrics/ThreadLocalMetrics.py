e7fb1e26c3d3f04005147e05d231f063976b0b05
souvik <souvik.ray@ril.com>
2016-05-01 18:51:05 +0530

    Making Metrics more generic and moving some common utility packages here

--- metrics/ThreadLocalMetrics.py
+++ metrics/ThreadLocalMetrics.py
@@ -7,84 +7,87 @@ Created on Jan 27, 2016
 import logging
 from logging.handlers import TimedRotatingFileHandler
 from abc import ABCMeta
-from AbstractMetrics import AbstractMetrics, AbstractMetricsFactory 
+from AbstractMetrics import AbstractMetrics, AbstractMetricsFactory
 import threading
 
-__BACKUP_COUNT = 100
-
-class LogRotationFrequency:
-    __metaclass__ = ABCMeta
-    ''''This prevents instantiation of the class since it is an interface. This
-    class however cannot be instantiated as it is an abstract class
-    '''
-        
-    SECONDS = 's'
-    MINUTE = 'm'
-    HOUR = 'h'
-    DAY = 'd'
-
-def create_timed_rotating_log(path, frequency = LogRotationFrequency.HOUR, interval = 1):
-    ''' This method describes the logging type of service logs
-    '''
-    # TODO: rotate to gzip format
-    logger = logging.getLogger("service.log")
-    handler = TimedRotatingFileHandler(path, frequency, interval, __BACKUP_COUNT)
-    logger.addHandler(handler)
-    return logger;
-        
-logger = create_timed_rotating_log("/var/log/cinder/service.log")
-
-        
 class ThreadLocalMetrics(AbstractMetrics):
     ''' Number of files to retain in the log folder
     '''
-    
+
     __threadLocal = threading.local()
-        
-    def __init__(self):
+
+    def __init__(self, logger):
         '''
         Constructor
         '''
         super(ThreadLocalMetrics, self).__init__()
         ThreadLocalMetrics.__threadLocal.metrics = self
-    
+        self.__logger = logger
+
     @staticmethod
     def get():
         return ThreadLocalMetrics.__threadLocal.metrics
-    
-    def _initialize_metrics(self, path, frequency, interval):
+
+    def _initialize_metrics(self):
         pass
- 
+
     def _flush_metrics(self):
-        ''' This just prints out the Metric object 
+        ''' This just prints out the Metric object
         '''
-        logger.info(self.__str__());
-        
+        self.__logger.info(self.__str__())
+
     def __str(self):
-        return super().__str__()
-    
+        return super(self).__str__()
+
     def close(self):
         super(ThreadLocalMetrics, self).close()
         ThreadLocalMetrics.__threadLocal.__dict__.clear()
-        
-           
+
+
+
+class Singleton(type):
+    def __init__(cls, name, bases, dic):
+        super(Singleton, cls).__init__(name, bases, dic)
+        cls.instance = None
+
+    def __call__(cls, *args, **kwargs):
+        if cls.instance is None:
+            cls.instance = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls.instance
+
 class ThreadLocalMetricsFactory(AbstractMetricsFactory):
     ''' Factory method to create Thread Local Metrics
     Example Usage:
-    metricsFactory = ThreadLocalMetricsFactory("/tmp/service_log").with_account_id("xxxxxxxxxxxxxxx").with_marketplace_id("IDC1").with_program_name("CinderAPI").with_operation_name("CreateVolume");
+    metricsFactory = ThreadLocalMetricsFactory("/tmp/service_log").with_marketplace_id("IDC1").with_program_name("CinderAPI")
     metrics =  metricsFactory.create_metrics();
     '''
-    
-    def __init__(self, service_log_path):
+    __metaclass__ = Singleton
+
+    def __init__(self, service_log_path, propagate_to_application_logs = True):
         super(ThreadLocalMetricsFactory, self).__init__()
-#        self.__service_log_path = service_log_path
-#        self.__interval = 1
-#        self.__frequency = LogRotationFrequency.HOUR
-        
-        
+        # This is done so that metrics flowing in cinder do not break.  To be removed soon.
+        service_log_path = service_log_path.replace("service_log", "service.log")
+        self.__logger = self.create_timed_rotating_log(service_log_path, propagate_to_application_logs)
+    '''
+    This method creates a thread local metrics
+    '''
     def create_metrics(self):
-        
-        metrics = ThreadLocalMetrics()
+        metrics = ThreadLocalMetrics(self.__logger)
         self._add_metric_attributes(metrics)
         return metrics
-   
+
+    #The default path is given to ensure backward compatibility to cinder. To be removed soon.
+    def create_timed_rotating_log(self, path = "/var/log/cinder/service.log", propagate_to_application_logs = True):
+        ''' This method describes the logging type of service logs
+        '''
+        logger = logging.getLogger("service.log")
+        logger.propagate = propagate_to_application_logs
+        # Uncomment this after thorough validation in Production, that all metrics are in service
+        # logs. This will remove the service logs from cinder API.
+
+        handler = logging.handlers.WatchedFileHandler(path)
+
+        # Cinder itself uses watched file handler. LogRotation is handled externally using logrotate.d
+
+        logger.addHandler(handler)
+        return logger
